752174d74ba6b7905caf901196869f9f5ac70520
bryanl <bryanliles@gmail.com>
2019-10-15 05:52:52 -0700

    select pod

--- src/app/components/pod-view/pod-view.component.ts
+++ src/app/components/pod-view/pod-view.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { DynamicComponent } from '../dynamic-component';
 import { PodStatus, PodView } from '../../view';
 
+const nodePrefix = 'node/';
+const podPrefix = 'pod/';
+
 @Component({
   selector: 'app-pod-view',
   templateUrl: './pod-view.component.html',
@@ -12,10 +15,6 @@ export class PodViewComponent extends DynamicComponent implements OnInit {
 
   @Input() view: PodView;
 
-  private podData = Array.from(Array(35), (_, x) => {
-    return { name: `pod-${x}`, value: 1, label: '' };
-  });
-
   constructor() {
     super();
   }
@@ -24,17 +23,38 @@ export class PodViewComponent extends DynamicComponent implements OnInit {
     this.data = this.nodeData();
   }
 
-  nodeData = () =>
+  labelFormat(data) {
+    if (data.label.startsWith(nodePrefix)) {
+      return data.label.substr(nodePrefix.length);
+    } else if (data.label.startsWith(podPrefix)) {
+      return data.label.substr(podPrefix.length);
+    }
+
+    console.log(data);
+    return data.label;
+  }
+
+  onSelect = (item: any) => {
+    if (item.name.startsWith(nodePrefix)) {
+      this.data = this.podData(item.name.substr(nodePrefix.length));
+      return;
+    }
+
+    console.log(`can't select`, item);
+  };
+
+  private nodeData = () =>
     Object.entries(
       this.view.config.pods.reduce((prev, cur) => {
         prev[cur.node] = (prev[cur.node] || 0) + 1;
         return prev;
       }, {})
     ).map(value => {
-      return { name: value[0], value: value[1] };
+      const name = `${nodePrefix}${value[0]}`;
+      return { name, value: value[1] };
     });
 
-  nodeColors = node => {
+  private nodeColors = node => {
     const pods = this.view.config.pods.filter(pod => pod.node === node);
     if (this.hasStatus(pods, 'error')) {
       return 'red';
@@ -44,6 +64,14 @@ export class PodViewComponent extends DynamicComponent implements OnInit {
 
     return 'green';
   };
+
+  private podData = (nodeName: string) =>
+    this.view.config.pods
+      .filter(pod => pod.node === nodeName)
+      .map(pod => {
+        const name = `${podPrefix}${pod.name}`;
+        return { name, value: 1 };
+      });
 
   private hasStatus = (pods: PodStatus[], status: string) => {
     return pods.find(pod => pod.status === status);
